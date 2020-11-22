52a987d6d2016b50dcfef2791682911603d4258d
zmerpez <37945511+zmerpez@users.noreply.github.com>
2020-11-21 23:35:55 -0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,15 +1,18 @@
 # author: Tiffany Timbers
-# date: 2020-01-15
+# modified by: Zeliha Ural Merpez
+# date: 2020-11-21
 
 """This script prints out docopt args.
-Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[--arg4=<arg4>]   Takes any value (this is an optional option)
 """
 
 from docopt import docopt
 opt = docopt(__doc__)
 print(opt)
 print(type(opt))
+print(opt["--arg4"])
